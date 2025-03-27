c45b68bd0045bf4c0249a22190400b043c8426a6
AlecCross <cross2472@gmail.com>
2025-03-27 14:46:03 +0000

    Add icons

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,17 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+        <Head>
+        <title>Stopwatch PWA</title>
+        <meta name="description" content="This is stopwatch PWA" />
+        {/* Іконки */}
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/x-icon" sizes="16x16" href="/icon-16.ico" />
+        <link rel="icon" type="image/x-icon" sizes="32x32" href="/icon-32.ico" />
+        
+        {/* Маніфест для PWA */}
+        <link rel="manifest" href="/manifest.json" />
+      </Head>
       <body>
         <Main />
         <NextScript />
