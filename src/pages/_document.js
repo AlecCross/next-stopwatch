19144f173f6fb6f7bd6a860e287884ec4a4abbcc
AlecCross <cross2472@gmail.com>
2025-05-05 09:24:51 +0000

    Change CR icons on 60

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -8,8 +8,9 @@ export default function Document() {
         <meta name="description" content="This is stopwatch PWA" />
         {/* Іконки */}
         <link rel="icon" href="/favicon.ico" />
-        <link rel="icon" type="image/x-icon" sizes="16x16" href="/icon-16.ico" />
-        <link rel="icon" type="image/x-icon" sizes="32x32" href="/icon-32.ico" />
+        <link rel="icon" href="/favicon.webp" />
+        <link rel="icon" type="image/x-icon" sizes="16x16" href="/icons/icon-16.ico" />
+        <link rel="icon" type="image/x-icon" sizes="32x32" href="/icons/icon-32.ico" />
         
         {/* Маніфест для PWA */}
         <link rel="manifest" href="/manifest.json" />
